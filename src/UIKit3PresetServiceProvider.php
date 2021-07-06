9087266ae6725a17c31c74378c2a7f2382e70a59
Michael Willis <willis@clarity-hd.com>
2021-07-06 12:41:10 -0600

    More Configuration changes for Laravel 8

--- src/UIKit3PresetServiceProvider.php
+++ src/UIKit3PresetServiceProvider.php
@@ -3,26 +3,26 @@
 namespace LaravelFrontendPresets\UIKit3Preset;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Foundation\Console\PresetCommand;
+use Laravel\Ui\UiCommand;
 
 class UIKit3PresetServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        PresetCommand::macro('uikit3', function ($command) {
-            $command->info('UIKit3 scaffolding installed successfully.');
-            $command->comment('Please run "npm install && npm run dev" to compile your fresh scaffolding.');
-            UIKit3Preset::install();
-        });
+  /**
+   * Bootstrap the application services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    UiCommand::macro('uikit3', function ($command) {
+      $command->info('UIKit3 scaffolding installed successfully.');
+      $command->comment('Please run "npm install && npm run dev" to compile your fresh scaffolding.');
+      UIKit3Preset::install();
+    });
 
-        PresetCommand::macro('uikit3-auth', function ($command) {
-            $command->info('UIKit3 auth scaffolding installed successfully.');
-            UIKit3Preset::addAuthTemplates();
-        });
-    }
+    UiCommand::macro('uikit3-auth', function ($command) {
+      $command->info('UIKit3 auth scaffolding installed successfully.');
+      UIKit3Preset::addAuthTemplates();
+    });
+  }
 }
